3cc68955c6fffcff33e648ee90975d4036fcd83b
Данил Буляков <bulyakov.hse@gmail.com>
2019-04-20 18:26:36 +0500

    Made SimpleInjector Container the default dependencyResolver of application

--- LoanPortfolio.WebApplication/Global.asax.cs
+++ LoanPortfolio.WebApplication/Global.asax.cs
@@ -12,13 +12,12 @@ using LoanPortfolio.Db.Repositories;
 using LoanPortfolio.Services;
 using LoanPortfolio.Services.Interfaces;
 using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
 
 namespace LoanPortfolio.WebApplication
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static Container _container;
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -26,27 +25,29 @@ namespace LoanPortfolio.WebApplication
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            _container = new Container();
+            Container container = new Container();
 
-            _container.Register(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
-            _container.RegisterInstance(typeof(DbContext), new LoanContext());
-            _container.Register<IUserService, UserService>();
-            _container.Register<IIncomeService, IncomeService>();
-            _container.Register<IExpenseService, ExpenseService>();
+            container.Register(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
+            container.RegisterInstance(typeof(DbContext), new LoanContext());
+            container.Register<IUserService, UserService>();
+            container.Register<IIncomeService, IncomeService>();
+            container.Register<IExpenseService, ExpenseService>();
 
-            _container.Verify();
+            container.Verify();
 
-            var userService = _container.GetInstance<IUserService>();
+            var userService = container.GetInstance<IUserService>();
             if (!userService.GetAll().Any())
             {
                 userService.Add("user@mail.ru", "password", "Ivan", "Ivanov");
             }
-            var expenseService = _container.GetInstance<IExpenseService>();
+            var expenseService = container.GetInstance<IExpenseService>();
             var user = userService.GetAll().ToList()[0];
             if (!expenseService.GetAll(user).Any(x => x.GetType() == typeof(HCSExpense)))
             {
                 expenseService.AddHCSExpense(user, DateTime.Now, 9999, "Комментарий");
             }
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
         }
     }
 }

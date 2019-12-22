577b108af61760326fdf593dc38941f5766c52ee
Данил Буляков <bulyakov.hse@gmail.com>
2019-12-22 14:55:03 +0500

    Added AuthService

---
+++ LoanPortfolio.Services.Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using LoanPortfolio.Db.Entities;
+
+namespace LoanPortfolio.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        bool IsLoggedIn();
+
+        User Login(string username, string password, bool isPersistent);
+
+        void Logout();
+    }
+}

6612749c2c3d7b5d16e97f3283c3a4db88e67c75
Reinger <dvbolotov@edu.hse.ru>
2020-03-04 18:35:25 +0500

    Change categories

--- LoanPortfolio.WebApplication/Controllers/CategoriesController.cs
+++ LoanPortfolio.WebApplication/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using LoanPortfolio.Db.Entities;
 using LoanPortfolio.Db.Interfaces;
+using LoanPortfolio.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +11,102 @@ namespace LoanPortfolio.WebApplication.Controllers
 {
     public class CategoriesController : Controller
     {
+        private User _user;
         private IRepository<Category> _category;
 
-        public CategoriesController(IRepository<Category> category)
+        public CategoriesController(IUserService userService, IRepository<Category> category)
         {
+            if (userService.GetAll().Any())
+            {
+                _user = userService.GetAll().ToList()[0];
+            }
+
             _category = category;
         }
 
 
-        public ActionResult Index(string name)
+        public ActionResult Index()
         {
-            if (name == null)
+            ViewBag.Title = "Категории";
+            ViewBag.Categories = _user.Categories;
+            return View();
+        }
+
+        public ActionResult AddCategories()
+        {
+            ViewBag.Title = "Новая категория";
+            ViewBag.Category = new Category();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategories(string name)
+        {
+            (List<string> errors, Category category) = Categories.CheckCategory(name);
+
+            if (_user.Categories.Where(x => x.Name == category.Name).ToList().Count > 0)
             {
-                ViewBag.Title = "Новая категория";
-                return View();
+                errors.Add("Такая категория уже существует");
             }
 
-            if (name.Length <= 0)
+            if (errors.Count == 0)
             {
-                ViewBag.Title = "Новая категория";
-                ViewBag.Error = "Введите название";
-                return View();
+                _category.Add(category);
+
+                ViewBag.Title = "Категории";
+                ViewBag.Categories = _user.Categories;
+                return View("Index");
             }
 
-            var categories = _category.All().Where(x => x.Name == name).ToList();
-            if (categories.Count > 0)
-            {
-                ViewBag.Title = "Новая категория";
-                ViewBag.Error = "Такая категория уже существует";
-                return View();
-            }
-            Category category = new Category();
-            category.Name = name;
-            _category.Add(category);
+            ViewBag.Title = "Новая категория";
+            ViewBag.Errors = errors;
+            return View();
+        }
 
-            ViewBag.Title = "Новый расход";
-            ViewBag.Category = _category.All().ToList();
-            return View("~/Views/Expense/AddPersonal.cshtml");
+        [HttpGet]
+        public ActionResult ChangeCategories(int id)
+        {
+            Category category = _user.Categories.Where(x => x.Id == id).First();
+            ViewBag.Title = category.Name;
+            ViewBag.Category = category;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangeCategories(int id, string name)
+        {
+            (List<string> errors, Category newCategory) = Categories.CheckCategory(name);
+            Category category = _user.Categories.Where(x => x.Id == id).First();
+
+            if (_user.Categories.Where(x => x.Name == newCategory.Name).ToList().Count > 0)
+            {
+                errors.Add("Такая категория уже существует");
+            }
+
+            if (errors.Count == 0)
+            {
+                category.Name = newCategory.Name;
+                _category.Update(category);
+
+                ViewBag.Title = "Категории";
+                ViewBag.Categories = _user.Categories;
+                return View("Index");
+            }
+
+            ViewBag.Title = category.Name;
+            ViewBag.Errors = errors;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            _category.Remove(_user.Categories.Where(x => x.Id == id).First());
+
+            ViewBag.Title = "Категории";
+            ViewBag.Categories = _user.Categories;
+
+            return View("Index");
         }
     }
 }
---
+++ LoanPortfolio.WebApplication/Utils/Categories.cs
@@ -0,0 +1,25 @@
+﻿using LoanPortfolio.Db.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanPortfolio.WebApplication
+{
+    public class Categories
+    {
+        static private bool ok;
+
+        //Добавление и изменение категорий расходов
+        public static (List<string> errors, Category category) CheckCategory(string name)
+        {
+            Category category = new Category();
+            List<string> errors = new List<string>();
+
+            (ok, category.Name) = Utils.CheckTextIsNotNull(name);
+            if (!ok) errors.Add("Введите название");
+
+            return (errors, category);
+        }
+    }
+}

b5f3ec3efc2390e5bdb935eecd08b4501537c129
Reinger <dvbolotov@edu.hse.ru>
2020-03-07 20:46:41 +0500

    Make authorization and registration

--- LoanPortfolio.WebApplication/Controllers/HomeController.cs
+++ LoanPortfolio.WebApplication/Controllers/HomeController.cs
@@ -12,8 +12,11 @@ namespace LoanPortfolio.WebApplication.Controllers
 {
     public class HomeController : BaseController
     {
+        public IUserService _userService;
+
         public HomeController(IUserService userService, IAuthService authService) : base(authService)
         {
+            _userService = userService;
         }
 
         public ActionResult Index()
@@ -36,7 +39,6 @@ namespace LoanPortfolio.WebApplication.Controllers
         public ActionResult Auth()
         {
             ViewBag.Title = "Авторизация";
-            ViewBag.User = CurrentUser;
 
             return View();
         }
@@ -44,16 +46,26 @@ namespace LoanPortfolio.WebApplication.Controllers
         [HttpPost]
         public ActionResult Auth(string login, string password)
         {
-            ViewBag.Title = "Кредитный портфель";
-            _authService.Login(login, password, true);
+            (List<string> errors, User user) = Users.CheckAuth(login, password, _userService.GetAll());
+            if (errors.Count == 0)
+            {
+                ViewBag.Title = "Кредитный портфель";
+                _authService.Login(user.Email, user.Password, true);
+                ViewBag.User = CurrentUser;
+                return View("Index");
+            }
+
+            ViewBag.Errors = errors;
+            ViewBag.Title = "Авторизация";
             ViewBag.User = CurrentUser;
-            return View("Index");
+
+            return View();
         }
 
         public ActionResult Register()
         {
             ViewBag.Title = "Регистрация";
-            ViewBag.User = CurrentUser;
+            ViewBag.User = new User();
 
             return View();
         }
@@ -61,10 +73,19 @@ namespace LoanPortfolio.WebApplication.Controllers
         [HttpPost]
         public ActionResult Register(string firstName, string lastName, string login, string password)
         {
-            ViewBag.Title = "Кредитный портфель";
-            ViewBag.User = CurrentUser;
+            (List<string> errors, User user) = Users.CheckRegister(firstName, lastName, login, password, _userService.GetAll());
+            if (errors.Count == 0)
+            {
+                _userService.Add(user.Email, user.Password, user.FirstName, user.LastName);
+                _authService.Login(user.Email, user.Password, true);
+                ViewBag.Title = "Кредитный портфель";
+                ViewBag.User = CurrentUser;
+                return View("Index");
+            }
 
-            return View("Index");
+            ViewBag.Title = "Регистрация";
+            ViewBag.User = user;
+            return View();
         }
     }
 }
---
+++ LoanPortfolio.WebApplication/Utils/Users.cs
@@ -0,0 +1,68 @@
+﻿using LoanPortfolio.Db.Entities;
+using LoanPortfolio.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanPortfolio.WebApplication
+{
+    public class Users
+    {
+        static private bool ok;
+
+        //Проверка при авторизации
+        public static (List<string> errors, User user) CheckAuth(string login, string password, IEnumerable<User> users)
+        {
+            User user = new User();
+            List<string> errors = new List<string>();
+
+            (ok, user.Email) = Utils.CheckTextIsNotNull(login);
+            if (!ok) errors.Add("Введите логин");
+
+            (ok, user.Password) = Utils.CheckTextIsNotNull(password);
+            if (!ok) errors.Add("Введите пароль");
+
+            if (errors.Count != 0) return (errors, user);
+
+            User user1 = users.SingleOrDefault(x => x.Email == user.Email);
+            if (user1 == null)
+            {
+                errors.Add("Неверный логин");
+            }
+            else
+            {
+                if (user1.Password != user.Password) errors.Add("Неверный пароль");
+            }
+
+            return (errors, user);
+        }
+
+        //Проверка при регистрации
+        public static (List<string> errors, User user) CheckRegister(string firstName, string lastName, string login, string password, IEnumerable<User> users)
+        {
+            User user = new User();
+            List<string> errors = new List<string>();
+
+            (ok, user.FirstName) = Utils.CheckTextIsNotNull(firstName);
+            if (!ok) errors.Add("Введите Имя");
+
+            (ok, user.LastName) = Utils.CheckTextIsNotNull(lastName);
+            if (!ok) errors.Add("Введите Фамилию");
+
+            (ok, user.Email) = Utils.CheckTextIsNotNull(login);
+            if (!ok) errors.Add("Введите Email");
+
+            (ok, user.Password) = Utils.CheckTextIsNotNull(password);
+            if (!ok) errors.Add("Введите пароль");
+
+            User user1 = users.SingleOrDefault(x => x.Email == user.Email);
+            if (user1 != null)
+            {
+                errors.Add("Пользователь с таким Email уже существует");
+            }
+
+            return (errors, user);
+        }
+    }
+}
